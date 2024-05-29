88c7e2e616e81b00ec7cd43dd94d98d0949dd63a
contenedorjem <154416968+contenedorjem@users.noreply.github.com>
2024-05-29 20:14:25 +0200

    Update WebConfig.java

--- src/main/java/com/jem/barataria/config/WebConfig.java
+++ src/main/java/com/jem/barataria/config/WebConfig.java
@@ -10,11 +10,10 @@ public class WebConfig implements WebMvcConfigurer {
     @SuppressWarnings("null")
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        // Configuración de CORS
-        registry.addMapping("/**") // Permite CORS para todas las rutas
-                .allowedOrigins("http://localhost:4200") // Añade aquí el origen de tu cliente Angular
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Métodos HTTP permitidos
-                .allowedHeaders("*") // Permite todos los headers
-                .allowCredentials(true); // Si necesitas enviar cookies o headers de autenticación
+        registry.addMapping("/api/**")
+                .allowedOrigins("https://barataria-8248826640a3.herokuapp.com")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
