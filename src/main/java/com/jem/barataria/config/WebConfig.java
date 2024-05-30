c31081844f52a5356becde2cf8bb264071651e32
contenedorjem <154416968+contenedorjem@users.noreply.github.com>
2024-05-30 13:29:18 +0200

    Update WebConfig.java

--- src/main/java/com/jem/barataria/config/WebConfig.java
+++ src/main/java/com/jem/barataria/config/WebConfig.java
@@ -7,11 +7,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    @SuppressWarnings("null")
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:4200", "https://barataria-8248826640a3.herokuapp.com", "https://barataria2-7c00aa53186c.herokuapp.com", "*")
+                .allowedOrigins("http://localhost:4200", "https://barataria-8248826640a3.herokuapp.com", "https://barataria2-7c00aa53186c.herokuapp.com")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
