b9b6f85d17319feb79444b8200c38a5526a6c9ab
contenedorjem <154416968+contenedorjem@users.noreply.github.com>
2024-05-30 13:13:45 +0200

    Update ForwardingController.java

--- src/main/java/com/jem/barataria/ForwardingController.java
+++ src/main/java/com/jem/barataria/ForwardingController.java
@@ -3,10 +3,17 @@ package com.jem.barataria;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+
 @Controller
 public class ForwardingController {
+
     @RequestMapping(value = "/**/{path:[^.]*}")
     public String redirect() {
+        return "forward:/index.html";
+    }
+
+    @RequestMapping(value = "/{path:[^\\.]*}", method = RequestMethod.GET)
+    public String redirectToIndex() {
         return "forward:/index.html";
     }
 }
