5d2a7f56d47e5b3c26a47866f374438986417dde
contenedorjem <154416968+contenedorjem@users.noreply.github.com>
2024-05-30 11:43:55 +0200

    Update WebConfig.java

--- src/main/java/com/jem/barataria/config/WebConfig.java
+++ src/main/java/com/jem/barataria/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("https://barataria-8248826640a3.herokuapp.com")
+                .allowedOrigins("http://localhost:4200", "https://barataria-8248826640a3.herokuapp.com", "*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
